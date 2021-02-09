d95edf2b0204691f3ced008f0a1d1e2610b5d435
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-09 14:45:24 -0600

    Added new contact add method...

---
+++ README.md
@@ -0,0 +1,16 @@
+# Contact CLI APP
+
+1. Create .txt file with base contact information and allow users to view information.
+
+Done.
+
+2. Create method that allows users to add to list.
+
+
+3. Create search function that allows users to search contact list for specific contact information.
+
+
+4. Create method that allows users to delete contact information.
+
+
+5. Finish method to allow users to exit program.
--- src/contactManager/contacts.java
+++ src/contactManager/contacts.java
@@ -9,9 +9,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class contacts {
+public class contacts extends Contact_Add {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Path contacts = Paths.get("contacts.txt");
 
         HashMap<String, String> contactInfo = new HashMap<String, String>();
@@ -20,41 +20,13 @@ public class contacts {
         contactInfo.put("Joshua T ", "345-8976");
         contactInfo.put("Casey F ", "237-7897");
 
-        //Update Contact Hashmap using user Input
-        String userInput;
-        String name;
-        String phone;
-        int contactsAdded = 0;
-        boolean valid = true;
 
-        try (Scanner scnr = new Scanner(System.in)) {
-            do {
-                System.out.println("Enter a name: ");
-                name = scnr.nextLine();
-
-                System.out.println("Enter the number for " + (name));
-                phone = scnr.nextLine();
-
-                // change made here
-                contactInfo.put(name, phone);
-
-                System.out.println("Would you like to enter another? (y/n) ");
-
-                contactsAdded++;
-
-                // here as well coz scanner was skipping the input
-            } while (scnr.nextLine().equalsIgnoreCase("y"));
-
-        }
-
-//        Files.write(contactPath, (Iterable<? extends CharSequence>) contactNames);
         File contactFile = new File(String.valueOf(contacts));
 
         BufferedWriter bf = null;
 
 
         try {
-
             //create new BufferedWriter for the output file
             bf = new BufferedWriter(new FileWriter(contactFile));
 
@@ -80,6 +52,4 @@ public class contacts {
             }
         }
     }
-
-
 }
